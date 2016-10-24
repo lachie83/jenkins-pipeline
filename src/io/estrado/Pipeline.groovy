5ac5e14ad035ab01a276580eab1421eec9c6f32b
Lachlan Evenson <lachlan@deis.com>
2016-10-24 16:00:54 -0700

    update helmDeploy

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -16,10 +16,7 @@ def helmDeploy(Map args) {
     //configure helm client and confirm tiller process is installed
     sh "/usr/local/linux-amd64/helm init"
 
-    // determine if application is released if not then install. This will be replaced with upgrade --install
-    sh "/usr/local/linux-amd64/helm status ${config.app.name} || /usr/local/linux-amd64/helm install ${pwd}/charts/croc-hunter --name ${config.app.name} --set ImageTag=${env.BUILD_NUMBER},Replicas=${config.app}.replicas,Cpu=${config.app.cpu},Memory=${config.app.memory} --namespace=${config.app.name}"
-    // If released then upgrade.
-    sh "/usr/local/linux-amd64/helm upgrade ${config.app.name} ${pwd}/charts/croc-hunter --set ImageTag=${env.BUILD_NUMBER},Replicas=${config.app.replicas},Cpu=${config.app.cpu},Memory=${config.app.memory}"
+    sh "/usr/local/linux-amd64/helm upgrade --install ${args.name} ${pwd}/charts/croc-hunter --set ImageTag=${args.build_number},Replicas=${args.replicas},Cpu=${args.cpu},Memory=${args.memory} --namespace=${args.name}"
 
-    echo "Application ${config.app.name} successfully deployed. Use helm status ${config.app.name} to confirm status"
+    echo "Application ${args.name} successfully deployed. Use helm status ${args.name} to check"
 }
